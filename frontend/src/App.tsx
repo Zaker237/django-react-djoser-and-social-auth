880b3391379c6118f7c0012642857b9cb7f2220e
Alex <mboutchouangalex+github@gmail.com>
2025-03-21 07:43:40 +0100

    init Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LoginPage } from './views/LoginPage'
+import { IndexPage } from './views/IndexPage'
 
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      Django React+TS Djoser and Social-Auth
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/views/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LoginForm } from "@/components/login-form"
+
+
+export const LoginPage: React.FC = () => {
+    return (
+        <div className="w-full max-w-sm">
+            <LoginForm />
+        </div>
+    )
+}
