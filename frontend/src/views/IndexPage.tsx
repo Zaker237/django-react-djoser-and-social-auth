90facfe29b0d319a7d781b3865daf4bd476ae7fc
Alex <mboutchouangalex+github@gmail.com>
2025-03-21 07:43:06 +0100

    init Index page

---
+++ frontend/src/views/IndexPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const IndexPage: React.FC = () => {
+    return (
+        <div className="w-full max-w-sm">
+            Django React+TS Djoser and Social-Auth
+        </div>
+    )
+}
