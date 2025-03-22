22da3595c7928840c29881cd23868b3e348bc104
Alex <mboutchouangalex+github@gmail.com>
2025-03-22 22:19:17 +0100

    desin sign up page

--- frontend/src/views/SignupPage.tsx
+++ frontend/src/views/SignupPage.tsx
@@ -74,7 +74,7 @@ export const SignupPage: React.FC = () => {
 									Forgot your password?
 								</a>
 								<Button type="submit" className="w-full">
-									Login
+									Sign up
 								</Button>
 								<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
 									<span className="relative z-10 bg-background px-2 text-muted-foreground">
