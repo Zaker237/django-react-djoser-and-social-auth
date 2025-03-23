e820928bfa71f9c43d30020d668cc97620adee6d
Alex <mboutchouangalex+github@gmail.com>
2025-03-23 22:37:39 +0100

    add auth interfaces

---
+++ frontend/src/interfaces/auths.d.ts
@@ -0,0 +1,11 @@
+export interface ICreateUser {
+    email: string
+    password: string
+    firstname: string
+    lastname: string
+}
+
+export interface ILoginUser {
+    email: string
+    password: string
+}
