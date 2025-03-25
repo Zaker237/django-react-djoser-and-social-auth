a0cc128ad5864776f4c4d3c3146be72d1a27b90a
Alex <mboutchouangalex+github@gmail.com>
2025-03-25 23:59:20 +0100

    signup

--- frontend/src/redux/actions/authAction.ts
+++ frontend/src/redux/actions/authAction.ts
@@ -2,15 +2,36 @@ import { IReduxAction } from "@/interfaces/store";
 import { Dispatch } from "redux";
 import { authActionTypes } from "@/constants/actionTypes";
 import { AuthService } from "@/services/AuthService";
-import { ILoginUser } from "@/interfaces/auths";
+import { ILoginUser, ICreateUser } from "@/interfaces/auths";
 
 export const login =
-  (user: ILoginUser) => async (dispatch: Dispatch<IReduxAction>) => {
-    dispatch({ type: authActionTypes.LOGIN });
-    try {
-      const response = await AuthService.login(user);
-      dispatch({ type: authActionTypes.LOGIN_SUCCESS, payload: response });
-    } catch (error) {
-      dispatch({ type: authActionTypes.LOGIN_FAILURE, error: "LOGIN FAILED" });
-    }
-  };
+	(user: ILoginUser) => async (dispatch: Dispatch<IReduxAction>) => {
+		dispatch({ type: authActionTypes.LOGIN });
+		try {
+			const response = await AuthService.login(user);
+			dispatch({ type: authActionTypes.LOGIN_SUCCESS, payload: response });
+		} catch (error) {
+			dispatch({ type: authActionTypes.LOGIN_FAILURE, error: (error as Error).message });
+		}
+	};
+
+
+export const signup =
+	(user: ICreateUser) => async (dispatch: Dispatch<IReduxAction>) => {
+		dispatch({ type: authActionTypes.SIGNUP });
+		try {
+			const response = await AuthService.signup(user);
+			dispatch({ type: authActionTypes.SIGNUP_SUCCESS, payload: response });
+		} catch (error) {
+			dispatch({ type: authActionTypes.SIGNUP_FAILURE, error: (error as Error).message });
+		}
+	}
+
+export const logout = () => async (dispatch: Dispatch<IReduxAction>) => {
+	dispatch({ type: authActionTypes.LOGOUT });
+	try {
+		dispatch({ type: authActionTypes.LOGOUT_SUCCESS });
+	} catch (error) {
+		dispatch({ type: authActionTypes.LOGOUT_FAILURE, error: (error as Error).message });
+	}
+}
--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,31 +1,31 @@
 import { ICreateUser, ILoginUser } from "@/interfaces/auths";
 
 export class AuthService {
-  public static async login(user: ILoginUser) {
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...user }),
-    });
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Login failed");
-  }
+	public static async login(user: ILoginUser) {
+		const response = await fetch("/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ...user }),
+		});
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error("Login failed");
+	}
 
-  public static async signup(user: ICreateUser) {
-    const response = await fetch("/api/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...user }),
-    });
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Signup failed");
-  }
+	public static async signup(user: ICreateUser) {
+		const response = await fetch("/api/signup", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ...user }),
+		});
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error("Signup failed");
+	}
 }
