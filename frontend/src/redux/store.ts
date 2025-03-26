ee055bf95e05b2b394fb3ecfcb433ea4e8b03bfe
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2025-03-27 00:36:42 +0100

    lint

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,15 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
 
 export const store = configureStore({
-	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // Includes thunk by default
-	devTools: process.env.NODE_ENV !== "production",
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // Includes thunk by default
+  devTools: process.env.NODE_ENV !== "production",
 });
 
-
 // Get the type of our store variable
-export type AppStore = typeof store
+export type AppStore = typeof store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
+export type RootState = ReturnType<AppStore["getState"]>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = AppStore['dispatch']
+export type AppDispatch = AppStore["dispatch"];
